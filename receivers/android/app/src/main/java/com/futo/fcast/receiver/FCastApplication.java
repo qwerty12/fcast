8a5e4b4d71008338f18609d131b2b0bf36187ca5
Faheem Pervez <trippin1@gmail.com>
2024-08-17 04:30:12 +0100

    Add Grayjay's 2.25 speed to ExoPlayer's selector
    (Even higher speeds are too much for this weak onn. box.)

---
+++ receivers/android/app/src/main/java/com/futo/fcast/receiver/FCastApplication.java
@@ -0,0 +1,55 @@
+package com.futo.fcast.receiver;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.annotation.OptIn;
+import androidx.media3.common.util.UnstableApi;
+import androidx.media3.ui.PlayerControlView;
+
+import java.lang.reflect.Constructor;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+
+public final class FCastApplication extends Application {
+    @OptIn(markerClass = UnstableApi.class)
+    static void ExoPlayerSpeedSelectorExtender() throws ClassNotFoundException, NoSuchMethodException {
+        final float[] PLAYBACK_SPEEDS_ADDITIONAL = {2.25f};
+        final String[] PLAYBACK_SPEED_TEXTS_ADDITIONAL = {"2.25x"};
+
+        final Class<?> clsPlaybackSpeedAdapter = Class.forName("androidx.media3.ui.PlayerControlView$PlaybackSpeedAdapter");
+        final Constructor<?> m = clsPlaybackSpeedAdapter.getDeclaredConstructor(PlayerControlView.class, String[].class, float[].class);
+
+        XposedBridge.hookMethod(m, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
+                final String[] playbackSpeedTexts = (String[]) param.args[1];
+                final float[] playbackSpeeds = (float[]) param.args[2];
+
+               /*if (playbackSpeedTexts == null || playbackSpeeds == null)
+                    return;*/
+
+                final String[] newPlaybackSpeedTexts = new String[playbackSpeedTexts.length + PLAYBACK_SPEED_TEXTS_ADDITIONAL.length];
+                final float[] newPlaybackSpeeds = new float[playbackSpeeds.length + PLAYBACK_SPEEDS_ADDITIONAL.length];
+
+                System.arraycopy(playbackSpeeds, 0, newPlaybackSpeeds, 0, playbackSpeeds.length);
+                System.arraycopy(PLAYBACK_SPEEDS_ADDITIONAL, 0, newPlaybackSpeeds, playbackSpeeds.length, PLAYBACK_SPEEDS_ADDITIONAL.length);
+
+                System.arraycopy(playbackSpeedTexts, 0, newPlaybackSpeedTexts, 0, playbackSpeedTexts.length);
+                System.arraycopy(PLAYBACK_SPEED_TEXTS_ADDITIONAL, 0, newPlaybackSpeedTexts, playbackSpeedTexts.length, PLAYBACK_SPEED_TEXTS_ADDITIONAL.length);
+
+                param.args[1] = newPlaybackSpeedTexts;
+                param.args[2] = newPlaybackSpeeds;
+            }
+        });
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        try {
+            ExoPlayerSpeedSelectorExtender();
+        } catch (final Throwable ignored) {}
+    }
+}
